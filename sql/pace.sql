bd373748829ad2797c05ecef126c0b722639defd
2115425Amar <amar2115425@akgec.ac.in>
2025-06-30 02:27:51 +0530

    sql

--- sql/pace.sql
+++ sql/pace.sql
@@ -4,3 +4,13 @@ WHERE date >= CURRENT_DATE - INTERVAL '3 months'
 GROUP BY customer_id
 ORDER BY total_spent DESC
 LIMIT 5;
+
+
+
+
+select customer_id
+from Customer
+group by customer_id
+having count(distinct product_key) = (
+    select count(*) from product
+)
